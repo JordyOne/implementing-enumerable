5a7fa319f7315a2f820965b4eee98756c6f960ac
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-07-31 10:29:13 -0600

    writing methods by hand

--- lib/gschool_enumerable.rb
+++ lib/gschool_enumerable.rb
@@ -1,3 +1,50 @@
 module GschoolEnumerable
+  def g_each # (block) for explicit
+    for item in self
+      yield(item) # yield control to the block and return itself implicit
+      # block.call(item) #explicit block
+    end
+    return self
+  end
+
+  def g_select
+    arr = []
+    self.g_each do |item|
+      arr << item if yield(item)
+    end
+    arr
+  end
+
+  def g_reject
+    arr = []
+    self.g_each do |item|
+      arr << item unless yield(item)
+    end
+    arr
+  end
+
+  def g_map
+    arr = []
+    self.g_each do |item|
+      arr << yield(item)
+    end
+    arr
+  end
+
+  def g_find
+    self.g_each do |item|
+      if yield(item)
+        return item
+      end
+    end
+    nil
+  end
+
+  def g_select!
+    self.g_each do |value|
+      self.delete(value) unless yield(value)
+    end
+    self
+  end
 
 end
